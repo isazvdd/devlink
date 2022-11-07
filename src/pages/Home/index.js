a2f1f842cd1b7de5d34a99be30f76b300456e320
Isadora Azevedo <isadora.azevedo.700@ufrn.edu.br>
2022-11-07 17:16:00 -0300

    updating files

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,30 @@
+import "./home.css";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Página HOME</h1>
+    <div className="home-container">
+      <h1>Isadora Azevedo</h1>
+      <span>Veja meus links 👇</span>
+
+      <main className="links">
+        <section className="link-area">
+          <a href="http://localhost:3000/isadora">
+            <p className="link-text">Canal no Youtube</p>
+          </a>
+        </section>
+
+        <section className="link-area">
+          <a href="http://localhost:3000/isadora">
+            <p className="link-text">Grupo privado no Telegram</p>
+          </a>
+        </section>
+
+        <section className="link-area">
+          <a href="http://localhost:3000/isadora">
+            <p className="link-text">Instagram</p>
+          </a>
+        </section>
+      </main>
     </div>
   );
 }

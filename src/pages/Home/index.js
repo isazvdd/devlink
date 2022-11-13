524eea803f0cfd02357822c259f6081ae5920cfd
Isadora Azevedo <isadora.azevedo.700@ufrn.edu.br>
2022-11-12 22:48:14 -0300

    adding new functions

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,11 @@
 import "./home.css";
+import { Social } from "../../components/Social";
+import {
+  FaWhatsapp,
+  FaInstagram,
+  FaGithub,
+  FaLinkedinIn,
+} from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -24,6 +31,24 @@ export default function Home() {
             <p className="link-text">Instagram</p>
           </a>
         </section>
+
+        <footer>
+          <Social url="https://wa.me/5584999919960">
+            <FaWhatsapp size={35} color="#FFF" />
+          </Social>
+
+          <Social url="https://instagram.com/luanazvdd">
+            <FaInstagram size={35} color="#FFF" />
+          </Social>
+
+          <Social url="https://github.com/isazvdd">
+            <FaGithub size={35} color="#FFF" />
+          </Social>
+
+          <Social url="https://www.linkedin.com/in/isazvdd/">
+            <FaLinkedinIn size={35} color="#FFF" />
+          </Social>
+        </footer>
       </main>
     </div>
   );
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,41 @@
+import { useState } from "react";
+
+import "./login.css";
+import { Logo } from "../../components/Logo";
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleLogin(e) {
+    e.preventDefault();
+
+    console.log(email);
+    console.log(password);
+  }
+
   return (
-    <div>
-      <h1>Página de Login</h1>
+    <div className="login-container">
+      <Logo />
+
+      <form className="form" onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Digite aqui o seu email..."
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="***********"
+          autoComplete="on"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Acessar</button>
+      </form>
     </div>
   );
 }

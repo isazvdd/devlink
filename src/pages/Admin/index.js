ae37c585f4449c1be27f6d2df678caf7238cb16b
Isadora Azevedo <isadora.azevedo.700@ufrn.edu.br>
2022-11-14 16:25:49 -0300

    managing data

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 import "./admin.css";
 
 import { Header } from "../../components/Header";
@@ -7,31 +9,133 @@ import { Input } from "../../components/Input";
 import { MdAddLink } from "react-icons/md";
 import { FiTrash2 } from "react-icons/fi";
 
+import { db } from "../../services/firebaseConection";
+import { toast } from "react-toastify";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  query,
+  orderBy,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
+
 export default function Admin() {
+  const [nameInput, setNameInput] = useState("");
+  const [urlInput, setUrlInput] = useState("");
+  const [backgroundColorInput, setBackgroundColorInput] = useState("#212121");
+  const [textColorInput, setTextColorInput] = useState("#ffffff");
+
+  const [links, setLinks] = useState([]);
+
+  useEffect(() => {
+    const linksRef = collection(db, "links");
+    const queryRef = query(linksRef, orderBy("created", "asc"));
+
+    const unsub = onSnapshot(queryRef, (snapshot) => {
+      let lista = [];
+
+      snapshot.forEach((doc) => {
+        lista.push({
+          id: doc.id,
+          name: doc.data().name,
+          url: doc.data().url,
+          bg: doc.data().bg,
+          color: doc.data().color,
+        });
+      });
+      setLinks(lista);
+    });
+  }, []);
+
+  async function handleRegister(e) {
+    e.preventDefault();
+
+    if (nameInput === "" || urlInput === "") {
+      toast.warn("Preencha todos os campos");
+      return;
+    }
+
+    addDoc(collection(db, "links"), {
+      name: nameInput,
+      url: urlInput,
+      bg: backgroundColorInput,
+      color: textColorInput,
+      created: new Date(),
+    })
+      .then(() => {
+        setNameInput("");
+        setUrlInput("");
+        toast.success("Link cadastrado com sucesso!");
+      })
+      .catch((error) => {
+        toast.error("Ops, algo deu errado. Tente novamente!");
+      });
+  }
+
+  async function handleDeleteLink(id) {
+    const docRef = doc(db, "links", id);
+    await deleteDoc(docRef);
+  }
+
   return (
     <div className="admin-container">
       <Header />
 
       <Logo />
 
-      <form className="form">
+      <form className="form" onSubmit={handleRegister}>
         <label className="label">Nome do link</label>
-        <Input placeholder="Nome do link" />
+        <Input
+          placeholder="Nome do link"
+          value={nameInput}
+          onChange={(e) => setNameInput(e.target.value)}
+        />
 
         <label className="label">URL do link</label>
-        <Input type="url" placeholder="Digite a URL do link" />
+        <Input
+          type="url"
+          placeholder="Digite a URL do link"
+          value={urlInput}
+          onChange={(e) => setUrlInput(e.target.value)}
+        />
 
         <section className="container-colors">
           <div>
             <label className="label right">Fundo do link</label>
-            <input type="color" />
+            <input
+              type="color"
+              value={backgroundColorInput}
+              onChange={(e) => setBackgroundColorInput(e.target.value)}
+            />
           </div>
 
           <div>
             <label className="label right">Cor do link</label>
-            <input type="color" />
+            <input
+              type="color"
+              value={textColorInput}
+              onChange={(e) => setTextColorInput(e.target.value)}
+            />
           </div>
         </section>
+
+        {nameInput !== "" && (
+          <div className="preview">
+            <label className="label">Veja como está ficando 👇</label>
+            <article
+              className="list"
+              style={{
+                marginBottom: 8,
+                marginTop: 8,
+                backgroundColor: backgroundColorInput,
+              }}
+            >
+              <p style={{ color: textColorInput }}> {nameInput} </p>
+            </article>
+          </div>
+        )}
 
         <button className="btn-register" type="submit">
           Cadastrar <MdAddLink size={24} color="#FFF " />
@@ -40,17 +144,23 @@ export default function Admin() {
 
       <h2 className="title">Meus links</h2>
 
-      <article
-        className="list animate-pop"
-        style={{ backgroundColor: "#000", color: "#FFF" }}
-      >
-        <p>Grupo exlusivo no Telegram</p>
-        <div>
-          <button className="btn-delete">
-            <FiTrash2 size={18} color="#FFF" />
-          </button>
-        </div>
-      </article>
+      {links.map((item, index) => (
+        <article
+          key={index}
+          className="list animate-pop"
+          style={{ backgroundColor: item.bg, color: item.color }}
+        >
+          <p>{item.name}</p>
+          <div>
+            <button
+              className="btn-delete"
+              onClick={() => handleDeleteLink(item.id)}
+            >
+              <FiTrash2 size={18} color="#FFF" />
+            </button>
+          </div>
+        </article>
+      ))}
     </div>
   );
 }

6f3a74121746e8e6f4c109dca03178c0832c2299
Isadora Azevedo <isadora.azevedo.700@ufrn.edu.br>
2022-11-14 20:47:26 -0300

    linked social networks

--- src/pages/Networks/index.js
+++ src/pages/Networks/index.js
@@ -1,7 +1,76 @@
+import { useState } from "react";
+import "./networks.css";
+
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+import { MdAddLink } from "react-icons/md";
+
+import { db } from "../../services/firebaseConection";
+import { setDoc, doc, getDoc } from "firebase/firestore";
+import { toast } from "react-toastify";
+
 export default function Networks() {
+  const [whatsapp, setWhatsapp] = useState("");
+  const [instagram, setInstagram] = useState("");
+  const [github, setGithub] = useState("");
+  const [linkedin, setLinkedin] = useState("");
+
+  function handleSave(e) {
+    e.preventDefault();
+
+    setDoc(doc(db, "social", "link"), {
+      whatsapp: whatsapp,
+      instagram: instagram,
+      github: github,
+      linkedin: linkedin,
+    })
+      .then(() => {
+        toast.success("Links salvos com sucesso!");
+      })
+      .catch((error) => {
+        toast.error("Erro ao salvar links. Tente novamente!");
+      });
+  }
+
   return (
-    <div>
-      <h1>rede social</h1>
+    <div className="admin-container">
+      <Header />
+
+      <h1 className="title-social">Suas redes sociais</h1>
+
+      <form className="form" onSubmit={handleSave}>
+        <label className="label"> Link do WhatsApp</label>
+        <Input
+          placeholder="Digite aqui a url do WhatsApp..."
+          value={whatsapp}
+          onChange={(e) => setWhatsapp(e.target.value)}
+        />
+
+        <label className="label"> Link do Instagram</label>
+        <Input
+          placeholder="Digite aqui a url do Instagram..."
+          value={instagram}
+          onChange={(e) => setInstagram(e.target.value)}
+        />
+
+        <label className="label"> Link do GitHub</label>
+        <Input
+          placeholder="Digite aqui a url do GitHub..."
+          value={github}
+          onChange={(e) => setGithub(e.target.value)}
+        />
+
+        <label className="label"> Link do LinkedIn</label>
+        <Input
+          placeholder="Digite aqui a url do LinkedIn..."
+          value={linkedin}
+          onChange={(e) => setLinkedin(e.target.value)}
+        />
+
+        <button type="submit" className="btn-register">
+          Salvar links <MdAddLink size={24} color="#FFF" />
+        </button>
+      </form>
     </div>
   );
 }
